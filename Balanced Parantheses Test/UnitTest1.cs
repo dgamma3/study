fb18eb041345e27a4dd670877461040fe074f6b1
daniel.brazil <daniel.brazil@xero.com>
2021-06-10 16:48:39 +1000

    u

--- Balanced Parantheses Test/UnitTest1.cs
+++ Balanced Parantheses Test/UnitTest1.cs
@@ -10,29 +10,7 @@ namespace Balanced_Parantheses_Test
         {
         }
 
-        [Test]
-        public void Test10()
-        {
-            var result = Result.minimumSwaps("(()))(");
-            Assert.AreEqual(result, 1);
-        }
-
-        [Test]
-        public void Test11()
-        {
-            var result = Result.minimumSwaps("((((((");
-            //var result = Result.minimumSwaps("((((((");
-            Assert.AreEqual(result, 3);
-        }
-
-        [Test]
-        public void Test12()
-        {
-            var result = Result.minimumSwaps("()())");
-            Assert.AreEqual(result, -1);
-        }
-
-        [Test]
+ [Test]
         public void Test1()
         {
             var result = Result.minimumSwaps("()");
@@ -80,6 +58,41 @@ namespace Balanced_Parantheses_Test
         {
             var result = Result.minimumSwaps(")(");
             Assert.AreEqual( 1,result);
+        }
+        
+        [Test]
+        public void Test8()
+        {
+            var result = Result.minimumSwaps("(()))(");
+            Assert.AreEqual(result, 1);
+        }
+
+        [Test]
+        public void Test9()
+        {
+            var result = Result.minimumSwaps("((((((");
+            Assert.AreEqual(result, 3);
+        }
+
+        [Test]
+        public void Test10()
+        {
+            var result = Result.minimumSwaps("()())");
+            Assert.AreEqual(result, -1);
+        }
+        
+        [Test]
+        public void Test11()
+        {
+            var result = Result.minimumSwaps(")))()()()()(((");
+            Assert.AreEqual(result, 3);
+        }      
+        
+        [Test]
+        public void Test12()
+        {
+            var result = Result.minimumSwaps(")()(((((()((()))()((");
+            Assert.AreEqual(result, 4);
         }
 
     }
